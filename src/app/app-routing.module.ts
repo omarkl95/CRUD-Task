02f84d3aa82c9a45e335b2ab6ee155d095d98c72
omar kamal <46107719+omarkl95@users.noreply.github.com>
2020-08-04 19:16:18 +0200

    Crud

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+
+const routes: Routes = [
+  {path:"",redirectTo:"signin",pathMatch:"full"},
+  {path:"signin",component:SignInComponent},
+  {path:"signup",component:SignUpComponent},
+  {path:"profile",component:ProfileComponent},
+  {path:"**",component:NotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
